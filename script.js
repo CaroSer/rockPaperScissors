54acd50a0c3a1a2a388696429acd98bebfc75643
Carolina Serrano <adrianaserrano186@outlook.com>
2023-06-13 23:23:33 -0600

    Finish the function of the game

--- script.js
+++ script.js
@@ -1,10 +1,57 @@
-
-    let playerChoice= prompt("Rock, paper or scissors?")
-    let computerChoice=randomChoice()
-
-
- function randomChoice(){
-    let options= ["Rock","Paper","Scissors"]
+function randomChoice(){
+    let options= ["rock","paper","scissors"]
     let randomChoice= Math.floor(Math.random()*(3.9-1)+1);
-    console.log(randomChoice)
- } 
+    let computerChoice=options[randomChoice]
+    return computerChoice
+} 
+
+function compareChoices(){
+    switch(true){
+
+        case(playerChoiceLC==computerChoice):
+        alert("It's a tie!");break;
+
+        case (playerChoiceLC=="rock"):
+            if(computerChoice=="paper"){ alert("You lose! Paper beats rock."); loses++}
+            else{ alert("You win! Rock beats scissors."); wins++;}
+            break;
+
+            case (playerChoiceLC=="paper"):
+            if(computerChoice=="scissors"){ alert("You lose! Scissors beats paper.");loses++}
+            else{ alert("You win! Paper beats rock."); wins++;}
+            break;
+
+            case (playerChoiceLC=="scissors"):
+            if(computerChoice=="rock"){ alert("You lose! Rock beats scissors.");loses++}
+            else{ alert("You win! Scissors beats paper."); wins++;}
+            break;
+
+            default: alert("Check your choice and try again.");break;
+    }
+}
+
+var wins=0;
+var loses=0;
+
+while(true){
+if(wins==5){
+    alert("You were the first to get 5 wins, you won!")
+    wins=0
+    loses=0
+    break;
+}
+
+else if(loses==5){
+    alert("Your opponent got 5 wins before you, you lost!")
+    wins=0
+    loses=0
+    break;
+}
+
+else{
+var playerChoice= prompt("Rock, paper or scissors?")
+var computerChoice=randomChoice() 
+var playerChoiceLC=playerChoice.toLowerCase()
+compareChoices()
+}
+}
